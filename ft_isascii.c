67c76dcd1f77d9dd821bcd70b06395c580ee9bd8
Natalia M <namatias@c3r5p1.42sp.org.br>
2025-07-24 19:34:16 -0300

    Criar isascii.c

---
+++ ft_isascii.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 19:21:46 by namatias          #+#    #+#             */
+/*   Updated: 2025/07/24 19:22:08 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isascii(int c)
+{
+	if (c >= 00 && c <= 127)
+		return (c);
+	return (0);
+}
+
+// int main()
+// {
+// 	printf("%d\n", ft_isascii(' '));
+// 	printf("%d\n", ft_isascii('a'));
+// 	printf("%d\n", ft_isascii('Z'));
+// 	printf("%d\n", ft_isascii('0'));
+// 	printf("%d\n", ft_isascii('9'));
+// 	printf("%d\n", ft_isascii('*'));
+// 	printf("%d\n", ft_isascii('{'));
+// 	printf("%d\n", ft_isascii('º'));
+// 	return (0);
+// }
