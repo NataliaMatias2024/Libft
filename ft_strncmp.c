5a8bac79e05ac27c419be331b79e3be019a00438
Natalia M <namatias@c2r4p6.42sp.org.br>
2025-07-27 16:31:19 -0300

    Rascunho da func. strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 15:58:10 by namatias          #+#    #+#             */
+/*   Updated: 2025/07/27 15:58:37 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+ int	ft_strncmp(const char *s1, const char *s2, size_t n)
+ {
+	size_t	i;
+
+ 	i = 0;
+ 	if (n == 0)
+ 		return (0);
+ 	while (s1[i] && s2[i] && i < n)
+ 	{
+
+ 		if (s1[i] != s2[i])
+ 			return (s1[i] - s2[i]);
+ 		i++;
+ 	}
+ 	return (s1[i] - s2[i]); 
+ }
+
+int main()
+{
+	char s1[] = "est";
+	char s2[] = "teste";
+	size_t n = 5;
+	printf("%d\n", ft_strncmp(s1, s2, n));
+}
