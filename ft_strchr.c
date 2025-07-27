085ed43f0eafef849f95cedb538f5e2b761626f1
Natalia M <namatias@c2r4p6.42sp.org.br>
2025-07-27 15:08:12 -0300

    Recriar func. strchr

---
+++ ft_strchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 14:35:49 by namatias          #+#    #+#             */
+/*   Updated: 2025/07/27 14:36:05 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (1)
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		if (s[i] == '\0')
+			break ;
+		i++;
+	}
+	return (NULL);
+}
+
+/*int	main()
+{
+	const char *s = "Socoooooooorro";
+	int c = 'c';
+
+	printf("%s\n", ft_strchr(s, c));
+}*/
