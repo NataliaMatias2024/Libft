b60cca5addf15ab8676b0f39b24bf9bced9487e3
Natalia M <namatias@c3r12p5.42sp.org.br>
2025-07-29 18:33:55 -0300

    Recriar bzero

---
+++ ft_bzero.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 18:31:13 by namatias          #+#    #+#             */
+/*   Updated: 2025/07/29 18:31:16 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	unsigned char	*ptr;
+
+//Fazer o cast (atribuir void *s para um unsigned char *)
+//permite acessar byte a byte do array
+	ptr = (unsigned char *)s;
+//O numero maximo de bytes que iremos zerar é n 
+//por isso ele será nosso criterio de parada.
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+}
+
+/*int main(void)
+{
+    char str[] = "Hello, world!";
+    int arr[5] = {10, 20, 30, 40, 50};
+    printf("Antes de ft_bzero - CHAR: %s\n", str);
+    printf("Antes de ft_bzero - INT:\n");
+    for (int i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    // Zerando os primeiros 5 bytes da string (ou seja, os 5 primeiros caracteres)
+    ft_bzero(str, 5);
+    ft_bzero(arr, 3 * sizeof(int));
+
+    printf("Depois de ft_bzero: ");
+    // Imprime byte a byte para mostrar zeros
+    for (size_t i = 0; i < sizeof(str); i++)
+    {
+        if (str[i] == 0)
+            printf("\\0");
+        else
+            printf("%c", str[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}*/
