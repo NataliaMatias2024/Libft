fcc7a8ab811492d8d7a87695f5359e2b3112be19
Natalia M <namatias@c3r12p5.42sp.org.br>
2025-07-29 14:07:53 -0300

    Update acrescenta unsigned char para resolver bugs em testes

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,35 +6,36 @@
 /*   By: namatias <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 15:58:10 by namatias          #+#    #+#             */
-/*   Updated: 2025/07/27 15:58:37 by namatias         ###   ########.fr       */
+/*   Updated: 2025/07/29 13:45:48 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
- int	ft_strncmp(const char *s1, const char *s2, size_t n)
- {
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
 	size_t	i;
 
- 	i = 0;
- 	if (n == 0)
- 		return (0);
- 	while (s1[i] && s2[i] && i < n)
- 	{
-
- 		if (s1[i] != s2[i])
- 			return (s1[i] - s2[i]);
- 		i++;
- 	}
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
 	if (i < n)
- 		return (s1[i] - s2[i]); 
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
- }
+}
 
-/*int main()
-{
-	char s1[] = "est";
-	char s2[] = "teste";
-	size_t n = 5;
-	printf("%d\n", ft_strncmp(s1, s2, n));
-}*/
+//embora n seja obrigado unsigned deve ser usado em comparações
+//binarias e de strings pois evita bugs com valores negativos
+// int main()
+// {
+// 	char s1[] = "abcdef";
+// 	char s2[] = "abc\375xx";
+// 	size_t n = 5;
+// 	printf("%d\n", ft_strncmp(s1, s2, n));
+// }
